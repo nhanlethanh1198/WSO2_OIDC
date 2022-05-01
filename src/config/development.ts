45349a56ac060f092c40c223d9ba6e85dece3fcc
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2022-05-01 15:51:58 +0700

    [Config]]

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,6 +1,21 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import sql from "./database";
+
 dotenv.config();
 
 export default {
-    PORT: process.env.PORT || 8000
-}
+  server: {
+    host: "localhost",
+    protocol: "http",
+    debug: true,
+    name: "LOCAL NAME",
+    port: 6500,
+    secret: process.env.SERVER_SECRET,
+  },
+  database: {
+    mongo: process.env.MONGODB_URI,
+    sessionSecret: process.env.SESSION_SECRET,
+    defaultPageSize: 50,
+    sql: sql.development,
+  },
+};
--- src/config/production.ts
+++ src/config/production.ts
@@ -1,6 +1,25 @@
 import * as dotenv from "dotenv";
+// import { config } from "../config";
+// const os = require("os");
+// import os from "os";
+// const sql = require("./database");
+import sql from "./database";
+
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT || 8000,
+  server: {
+    host: process.env.HOST_NAME,
+    protocol: "http",
+    debug: true,
+    name: "SERVER NAME",
+    port: process.env.PORT || 6000,
+    secret: process.env.SERVER_SECRET,
+  },
+  database: {
+    mongo: process.env.MONGOLAB_URI,
+    sessionSecret: process.env.SESSION_SECRET,
+    defaultPageSize: 50,
+    sql: sql.production,
+  },
 };
