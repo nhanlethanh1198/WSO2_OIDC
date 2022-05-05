a02d5bcc64635f123fe1dc60f965ea0c7f31279e
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2022-05-05 17:52:42 +0700

    oauth

---
+++ src/routes/oauth.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    const {headers, body} = req;
+
+    res.send({
+        headers,
+        body
+    })
+
+})
+
+router.post("/", (req, res, next) => {
+    const {headers, body} = req;
+
+    res.send({
+        headers,
+        body
+    })
+
+})
+
+
+router.put("/", (req, res, next) => {
+    const {headers, body} = req;
+
+    res.send({
+        headers,
+        body
+    })
+
+})
+
+
+router.delete("/", (req, res, next) => {
+    const {headers, body} = req;
+
+    res.send({
+        headers,
+        body
+    })
+
+})
+
+
+
+module.exports = router;
